425002dfc36018f08e2ca45325ca3e1c4432f159
escorialxd <escorialxd@gmail.com>
2024-07-30 13:38:55 +0300

    Refactored file configuration.

---
+++ blogicum/pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def about(request):
+    """Страница 'О нас'."""
+    return render(request, 'about.html')
+
+
+def rules(request):
+    """Страница с правилами."""
+    return render(request, 'rules.html')
