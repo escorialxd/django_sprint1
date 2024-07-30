43cbf040d2bc4ce7e8570f74f8db074ceb3c4d50
escorialxd <escorialxd@gmail.com>
2024-07-30 13:38:20 +0300

    Refactored file configuration.

--- blogicum/urls.py
+++
@@ -1,9 +0,0 @@
-"""Ыыыыы."""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('pages/', include('pages.urls')),
-]
